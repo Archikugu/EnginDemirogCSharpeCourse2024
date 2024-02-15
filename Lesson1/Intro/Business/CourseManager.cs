ffc42314462c7777653e5aedf57fb8acd2a81231
Gokhan <engokhangok@gmail.com>
2024-02-15 12:05:13 +0300

    Add project files.

---
+++ Lesson1/Intro/Business/CourseManager.cs
@@ -0,0 +1,39 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business
+{
+    public class CourseManager
+    {
+        Course[] courses = new Course[2];
+        public CourseManager()
+        {
+            Course course1 = new Course();
+            course1.Id = 1;
+            course1.Name = "C#";
+            course1.Description = ".NET 8 vs ...";
+            course1.Price = 0;
+
+            Course course2 = new Course();
+            course2.Id = 2;
+            course2.Name = "Java";
+            course2.Description = "Java 17 ....";
+            course2.Price = 0;
+
+            courses[0] = course1;
+            courses[1] = course2;
+        }
+        public Course[] GetAll()
+        {
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine(courses[i].Name + " / " + courses[i].Description);
+            }
+            return courses;
+        }
+    }
+}
---
+++ Lesson1/Intro/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message = "Krediler";
+Console.WriteLine(message);
+
+int term = 12;
+double amount = 1000000.5;
+
+//variables --> camelCase
+bool isAuthenticated = false;
+
+//condition
+if (isAuthenticated)
+{
+    Console.WriteLine("Buton-->Hoşgeldin Gökhan");
+}
+else
+{
+    Console.WriteLine("Buton-->Sisteme Giriş Yap");
+}
+
+string credit1 = "Kredi 1";
+string credit2 = "Kredi 2";
+string credit3 = "Kredi 3";
+string credit4 = "Kredi 4";
+
+Console.WriteLine(credit1);
+Console.WriteLine(credit2);
+Console.WriteLine(credit3);
+Console.WriteLine(credit4);
+
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4" };
+//string[] loans2 = new string[6];
+//loans2[0] = "Kredi 1";
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+foreach (var loan in loans)
+{
+    Console.WriteLine(loan);
+}
+
+CourseManager courseManager = new();
+courseManager.GetAll();
+
+Console.WriteLine("Kod Bitti");
