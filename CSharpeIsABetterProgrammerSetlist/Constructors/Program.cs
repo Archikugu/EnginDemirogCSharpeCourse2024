59306d17f9d5f7065dae919068bfa2164f305b3a
Gokhan <engokhangok@gmail.com>
2024-02-25 20:24:02 +0300

    Day 2 Task 2 C# is a Better Programmer Setlist is Completed

---
+++ CSharpeIsABetterProgrammerSetlist/Constructors/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Customer customer = new Customer();
+
+Customer customer2 = new Customer { ID = 1, FirstName = "Gökhan", LastName = "Gök", City = "Ankara" };
+Customer customer3 = new Customer(2, "Osman", "Biçer", "Ankara");
+
+Console.WriteLine(customer3.FirstName);
+
+class Customer
+{
+    //Default Constructor
+    public Customer()
+    {
+        Console.WriteLine("Yapıcı Blok Çalıştı");
+    }
+    public Customer(int id, string firstName, string lastName, string city)
+    {
+        ID = id;
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+    }
+    public int ID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
